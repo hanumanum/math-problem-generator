4597b6fdf8205e4483478dc0c49aa524547bfe1c
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-05-25 08:25:22 +0000

    types of problems added

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-let eq = document.getElementById("eq")
-eq.innerHTML = newEq()
-MathJax.Hub.Typeset()
+let eq = document.getElementById("eq");
+let levels = document.getElementById("levels");
+
+//TODO: remove later, just for testing
+//levels.value = 2;
+
+eq.innerHTML = newEq(levels.value);
+MathJax.Hub.Typeset();
 
 
-let levels = document.getElementById("levels")
 levels.addEventListener("change",function(){
-    eq.innerHTML = newEq()
-    MathJax.Hub.Typeset()
+    eq.innerHTML = newEq(levels.value);
+    MathJax.Hub.Typeset();
 })
 
-
-function newEq(){
+function newEq(level){
+    console.log(level);
+    let ops = ["+","-"];
     let n1 =  randomInt(5,30);
     let n2 =  randomInt(4,30);
     let n3 =  randomInt(2,30);
     let p =  randomInt(4,8);
+    let op1 = randomInt(0,2);
+    let op2 = randomInt(0,2);
 
-    return "$$x = \\frac{"+n1+"a^"+p+"+"+n2+"b+c} {"+n3+"}$$"
+    if(level==="1"){
+        return "$$x = "+n1+"a^"+p+ops[op1]+n2+"b"+ops[op2]+"c$$";
+    }
+    else if (level==="2"){
+        return "$$x = "+n1+ "\\sqrt{a}"+ops[op1]+n2+"b"+ops[op2]+"c$$";
+
+    }
+    else if (level==="3"){
+        return "$$x = \\frac{"+n1+"a^"+p+"+"+n2+"b+c} {"+n3+"}$$";
+    }
 }
 
 
